a71ea7c55196ccec0ee956dd1333dec0ea762f28
YULIAN E PEREZ BENAVIDEZ <yulbenavidez@gmail.com>
2025-02-22 14:09:52 -0500

    Subidas de practica

---
+++ Practica01PWA/Controllers/CalculadoraController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica01PWA.Models;
+
+namespace Practica01PWA.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Operaciones();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Operaciones model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.TipoOperacionId == 1)
+                {
+                    model.Resultado = model.PrimerDato + model.SegundoDato;
+                } else if (model.TipoOperacionId == 2)
+                {
+                    model.Resultado = model.PrimerDato - model.SegundoDato;
+                } else if (model.TipoOperacionId == 3)
+                {
+                    model.Resultado = model.PrimerDato * model.SegundoDato;
+                } else if (model.TipoOperacionId == 4)
+                {
+                    if (model.SegundoDato !=0)
+                    {
+                        model.Resultado = model.PrimerDato / model.SegundoDato;
+                    }
+                }
+            }
+            return View(model);
+
+        }
+    }
+}
---
+++ Practica01PWA/Models/TiposOperacion.cs
@@ -0,0 +1,8 @@
+﻿namespace Practica01PWA.Models
+{
+    public class TiposOperacion
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
